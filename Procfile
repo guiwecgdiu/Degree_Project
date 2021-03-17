1930d7dd91cc958595e9742d51eb90e3c521b7ec
Arno Li <guiwecgdiu@163.com>
2021-03-17 17:38:05 +0800

    1111

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: flask db upgrade && gunicorn --access-logfile - --error-logfile - -w 4 "backend:create_app()"
--- frontend/src/store/todos/index.js
+++
@@ -1,8 +0,0 @@
-import mutations from './mutations'
-import actions from './actions'
-
-export default {
-  state: [],
-  mutations,
-  actions
-}
